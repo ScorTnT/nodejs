bf429fbc5e956a61b54d032d524fc460b38ec87f
ScorTnT <flalxlemdkdlel@naver.com>
2021-04-08 19:59:14 +0900

    04-08-desktop

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ let app = http.createServer(function(req, res){
     if(pathname=="/web"){
 
         const queryObject = url.parse(req.url,true).query;
-
         var data = fs.readFileSync('./data.json', 'utf8');
         const dat = JSON.parse(data);
-
         let litag = ""
         dat.map(function(item, idx){
             litag +=  `
@@ -29,7 +27,6 @@ let app = http.createServer(function(req, res){
                     <head>
                         <title>WEB1 - Welcome</title>
                         <meta charset="utf-8">
-
                     </head>
                     <body>
                         <h1><a href="/web">WEB</a></h1>
@@ -47,13 +44,11 @@ let app = http.createServer(function(req, res){
         else {
             const title = dat[queryObject.id].title
             const description = dat[queryObject.id].description
-
             res.end(`            
             <html>
                 <head>
                     <title>WEB1 - ${title} </title>
                     <meta charset="utf-8">
-
                 </head>
                 <body>
                     <h1><a href="/web">WEB</a></h1>
@@ -66,7 +61,6 @@ let app = http.createServer(function(req, res){
                     <a href="/web/create"><button type="button">Create!</button></a>
                     <a href="/web/update?id=${queryObject.id}"><button type="button">Update!</button></a>
                     <a href="/web/delete?id=${queryObject.id}"><button type="button">Delete!</button></a>
-
                 </body>
             </html>
             `)
@@ -91,7 +85,6 @@ let app = http.createServer(function(req, res){
         `)
     }
     else if(pathname=="/web/create_process"){ //create_process 로 접속
-
         var jsonData = "";  //jsonData => 문자열, 입력한 title, description 저장위함.
         req.on('data', function (chunk) { //req.on('data',) => post data 조각들이 서버에 도착할 때 callback 함수를 실행. chunk => data 조각들 
           jsonData += chunk;    // data조각들을 누적해나감
@@ -100,17 +93,14 @@ let app = http.createServer(function(req, res){
             const partdata = jsonData.split('&description=')   // jsonData를 찢어서 partdata로 설정함. { title, description 으로 나누기위한 1단계 }
             const titledata = partdata[0].split('title=')[1]  // partdata의 'title='뒷부분을 titledata로 설정
             const descriptiondata = partdata[1]                       // partdata에서 'description=' 뒷부분을 descriptiondata로 설정
-
             var data = fs.readFileSync('./data.json', 'utf8'); // /data.json 파일을 한글형식제공으로 읽어들이고 data 라는 변수로 설정
             const dat = JSON.parse(data);                      // 설정된 data 문자열 전체를 JSON 형식으로 변환함
-
             dat.push(                         // 사용자가 입력한 title, description을 하나의 객체로 묶어 dat배열에 마지막 부분에 push(삽입) 하기 위해서 쓰여짐
                 {
                     "title": titledata ,
                     "description": descriptiondata
                 }                                  // 기존 형식에 맞게 push(삽입)
                 )                                  // push(삽입)완료
-
             const filteredArr = dat.reduce((acc, current) => {               // 빈배열 acc를 설정하고 각 배열의 요소(current)를 반복함 { 중복제거 } => 최종 결과물을 filteredArr[]로 설정 
                 const x = acc.find(item => item.title === current.title );    // title과 description 요소들을 찾아 비교하여 x라 칭함
                 if (!x) {                           // 다르면
@@ -122,10 +112,9 @@ let app = http.createServer(function(req, res){
                     res.end();                                      // 종료
                 }
               }, []);
-              
+
             const newdata = JSON.stringify(filteredArr)     // 최종 결과물 filteredArr[]을 JSON형식 문자열로 변환 후 newdata라 칭함
             fs.writeFileSync('./data.json',newdata)         // 문자열 newdata를 ./data.json 파일에 덮어 씌움
-
             res.statusCode = 302;                           // 서버가 사용자에게 강제로 페이지를 옮기라고 지시하기 위한 statusCode.
 	        res.setHeader('Location', '/web');              // 강제로 옮겨질 페이지의 주소가 setHeader 매개변수에 쓰여있어야 하며 그 주소는 '/web'임
  	        res.end();                                      // 종료
@@ -135,11 +124,9 @@ let app = http.createServer(function(req, res){
 
     // update + process
     else if(pathname==="/web/update") {
-
         const queryObject = url.parse(req.url,true).query;
         var data = fs.readFileSync('./data.json', 'utf8');
         const dat = JSON.parse(data);
-
         const title = dat[queryObject.id].title
         const description = dat[queryObject.id].description
         res.end(`
@@ -154,7 +141,6 @@ let app = http.createServer(function(req, res){
             </form>
         </body>
         `)
-
     }
     else if(pathname==="/web/update_process") {
         var jsonData = "";  //jsonData => 문자열, 입력한 title, description 저장위함.
@@ -167,16 +153,12 @@ let app = http.createServer(function(req, res){
             const part2 = part1[0].split('&description=')
             const des = part2[1]
             const tit = part2[0].split('title=')[1]
-
             var data = fs.readFileSync('./data.json', 'utf8');
             const dat = JSON.parse(data);
-            
             dat[idx].title = tit 
             dat[idx].description = des
-
             const newdata = JSON.stringify(dat)
             fs.writeFileSync('./data.json',newdata)
-
             res.statusCode = 302;                           // 서버가 사용자에게 강제로 페이지를 옮기라고 지시하기 위한 statusCode.
 	        res.setHeader('Location', '/web');              // 강제로 옮겨질 페이지의 주소가 setHeader 매개변수에 쓰여있어야 하며 그 주소는 '/web'임
  	        res.end();                                      // 종료
@@ -212,12 +194,10 @@ let app = http.createServer(function(req, res){
     }
 
 
-
     // error or undefined 
     else {
         return res.writeHead(404)
     }
 });
-
 
 app.listen(3000);
